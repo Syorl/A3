ae59c9714c8aa64e8270e1fd579b9c9576b5237a
JHRG-cod <jorgegandolfiestudos@gmail.com>
2024-12-03 22:40:26 -0300

    Atualização docker e banco

--- db.js
+++ db.js
@@ -1,21 +1,31 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+const mysql = require("mysql2");
+require("dotenv").config();
 
-// Conexão com o banco de dados
+// Configurar a conexão com o banco de dados
 const db = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "senha123",
   database: process.env.DB_NAME || "SistemaGerenciamento",
-  port: process.env.DB_PORT || 3306 // Adicionando a configuração da porta
+  port: process.env.DB_PORT || 3306,
 });
 
+// Conectar ao banco de dados
 db.connect((err) => {
   if (err) {
     console.error("Erro ao conectar no banco de dados:", err.message);
     process.exit(1);
   }
   console.log("Conexão com o banco de dados estabelecida com sucesso!");
+
+  // Testar consulta simples para verificar conexão
+  db.query("SELECT 1 + 1 AS resultado", (err, results) => {
+    if (err) {
+      console.error("Erro ao testar conexão com o banco:", err.message);
+      return;
+    }
+    console.log("Conexão testada com sucesso! Resultado:", results[0].resultado);
+  });
 });
 
 module.exports = db;
