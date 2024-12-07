5e2a873d0918cfd8bd28c4211f4345280aa7872b
Guilherme Ornellas <112431864+Syorl@users.noreply.github.com>
2024-12-06 22:40:30 -0300

    html de pedidos

--- controllers/pedidosController.js
+++ controllers/pedidosController.js
@@ -3,7 +3,7 @@ const pool = require('../db');
 // Buscar todos os pedidos
 const getAllPedidos = async (req, res) => {
   const query = `
-    SELECT p.id_pedido, c.nome AS cliente, v.nome AS vendedor, pr.nome AS produto, p.quantidade, p.valor
+    SELECT p.id_pedido, c.nome AS cliente, v.nome AS vendedor, pr.nome AS produto, p.quantidade, p.valor, p.id_cliente, p.id_produto, p.id_vendedor
     FROM pedidos p
     JOIN Clientes c ON p.id_cliente = c.id_cliente
     JOIN Vendedores v ON p.id_vendedor = v.id_vendedor
@@ -47,11 +47,11 @@ const updatePedido = async (req, res) => {
   const { id_cliente, id_vendedor, id_produto, quantidade, valor } = req.body;
   const query = `
     UPDATE pedidos 
-    SET id_cliente = ?, id_vendedor = ?, id_produto = ?, quantidade = ?, valor = ? 
+    SET quantidade = ?, valor = ? 
     WHERE id_pedido = ?
   `;
   try {
-    const [result] = await pool.query(query, [id_cliente, id_vendedor, id_produto, quantidade, valor, id]);
+    const [result] = await pool.query(query, [ quantidade, valor, id]);
     if (result.affectedRows > 0) {
       res.json({ id_pedido: id, id_cliente, id_vendedor, id_produto, quantidade, valor });
     } else {
